23af944ee629553c0b8f22aab2e303453bf54a01
taiji9203 <taiji9203@naver.com>
2022-11-26 13:57:43 +0900

    저장3

--- java_project/src/ch03_test_1018/Hello2.java
+++ java_project/src/ch03_test_1018/Hello2.java
@@ -10,7 +10,7 @@ public class Hello2 {
 
 		// 여러 행 주석 : ctrl + shift + /
 		// 여러 행 주석 해제 : ctrl + shift + \
-		// 자동 포맷 정렬 : ctrl + shift + f ...
+		// 자동 포맷 정렬 : ctrl + shift + f .....
 		System.out.println("Hello 이상용 자바");
 		System.out.println("Hello 이상용 자바");
 
